27aec278d48c6131b76d1790fd170c55906fa543
karahmon <monilkarania@gmail.com>
2024-03-26 20:46:01 +0530

    first commit

--- 08minicontext/src/components/Login.jsx
+++ 08minicontext/src/components/Login.jsx
@@ -2,6 +2,9 @@ import React,{useState,useContext} from 'react';
 import UserContext from '../context/UserContext';
 
 function Login(){ 
+    const [username,setUsername]=useState("")
+    const [password,setPassword]=useState("")
+    const handlesubmit=()=>{}
     return (
         <>
         <div>
