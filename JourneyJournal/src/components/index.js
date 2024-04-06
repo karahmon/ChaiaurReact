d758c287e4eda81843d3729a254d9bf89e747358
karahmon <monilkarania@gmail.com>
2024-04-06 22:16:36 +0530

    Journey Journal Part 2

--- JourneyJournal/src/components/index.js
+++ JourneyJournal/src/components/index.js
@@ -3,6 +3,7 @@ import Container from "./container/Container"
 import Logo from "./Logo"
 import Header from "./Header/Header"
 import LogoutBtn from "./Header/LogOutButton"
+import Button from "./Header/Button"
 
 
-export {Header,Footer,Container,Logo,LogoutBtn};
+export {Header,Footer,Container,Logo,LogoutBtn,Button};
