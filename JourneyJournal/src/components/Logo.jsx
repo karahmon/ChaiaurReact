819666a359d1a8458e57ce5bafccccf45c73cf13
karahmon <monilkarania@gmail.com>
2024-04-09 19:09:14 +0530

    final commit

---
+++ JourneyJournal/src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <div className="logo-main">
+  <img alt="logo" src="https://cloud.appwrite.io/v1/storage/buckets/JourneyJournalImages/files/661540a366d760e8e80e/view?project=journeyjournal&mode=admin"/>
+</div>
+  )
+}
+
+export default Logo
